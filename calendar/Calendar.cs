070facd5231d05decd5f3bbb42c1b3003017754a
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 20:51:31 +0200

    Add project files.

---
+++ calendar/Calendar.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calendar
+{
+    public class Calendar
+    {
+        public enum lunaAnului
+        {
+            Ianuarie = 1,
+            Februarie, Martie, Aprilie, Mai, Iunie, Iulie,
+            August, Septembrie, Octombrie, Noiembrie, Decembrie
+        }
+        public string [] denumireaLunilorAnului = {"Ianuarie" ,
+            "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie",
+            "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"};
+
+        public int[] numaruluiZilelorInFiecareLuna = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+        public int[] numaruluiZilelorInFiecareLunaInAnBisect = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        public int nrZilelorAnului = 365;
+        public int nrZilelorAnuluiBisect = 366;
+
+        public int gasesteNumarulZilei(Calendar.lunaAnului x)
+        {
+            return (int)x;
+        }
+
+        public DateTime OraCurenta = DateTime.Now;
+        public DateTime TimpCurent = DateTime.Now;
+
+        public void UpdateazaOraCurenta()
+        {
+            OraCurenta = DateTime.Now;
+        }
+        public void UpdateazaTimpCurent()
+        {
+            TimpCurent = DateTime.Now;
+        }
+
+        public string afiseazaToateZileleSaptamanii()
+        {
+            String rez = "";
+            for (int i = 0; i < denumireaLunilorAnului.Length; i++)
+            {
+                rez += denumireaLunilorAnului[i].ToString() + " : " + (i+1).ToString() + " , \r\n" ;
+            }
+
+            return rez;
+        }
+
+        public string afiseazaOraCurenta()
+        {
+
+            return OraCurenta.ToLongTimeString(); 
+
+        }
+
+        public string afiseazaDataCurenta()
+        {
+
+            return OraCurenta.Date.ToString();
+
+        }
+
+        public string afiseazaTimpulCurent()
+        {
+            string rez="";
+
+            TimpCurent = DateTime.Now;
+            int H = TimpCurent.Hour;
+            int M = TimpCurent.Minute;
+            int S = TimpCurent.Second;
+            int MS = TimpCurent.Millisecond;
+
+            rez += H + ":";
+            rez += M + ":";
+            rez += S + ":";
+            rez += MS ;
+          
+
+            return rez;
+        }
+
+    }
+}
---
+++ calendar/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace calendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        public void afisareDataTimp()
+        {
+            Calendar c = new Calendar();
+
+            /*
+            Text = c.gasesteNumarulZilei(Calendar.lunaAnului.Ianuarie).ToString();
+            MessageBox.Show(c.afiseazaToateZileleSaptamanii());
+            MessageBox.Show(c.afiseazaOraCurenta());
+            MessageBox.Show(c.afiseazaTimpulCurent());
+            MessageBox.Show(c.afiseazaDataCurenta());
+            */
+
+            for (; ; )
+            {
+
+
+                c.UpdateazaOraCurenta();
+                //Text = c.OraCurenta.ToString();
+                //MessageBox.Show( c.OraCurenta.ToString());
+
+                Thread.Sleep(10);
+            }
+
+        }
+
+
+        public delegate void MyDelegate();
+
+
+        void RunlnThread()
+        {
+            MyDelegate dellnstatnce = new MyDelegate(Addcontrol);
+            this.Invoke(dellnstatnce);
+            MessageBox.Show("Hello");
+
+        }
+        void Addcontrol()
+        {
+            TextBox textBox1 = new TextBox();
+            Controls.Add(textBox1);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+
+
+            //Thread x = new Thread(new ThreadStart(afisareDataTimp));
+            // x.Start();
+
+
+
+
+            Thread t = new Thread(new ThreadStart(RunlnThread));
+            t.Start();
+
+
+
+        }
+    }
+}
